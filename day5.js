3b925f2eede9e97be5a3f0d7aff1fc2bf01600dc
Emily Wenberg <martiansnoop@gmail.com>
2019-12-14 12:19:28 -0800

    Finish day5 part 1; implement parameter modes in the intcode computer

--- day5.js
+++ day5.js
@@ -9,6 +9,17 @@ if (require.main === module) {
     test("1,9,10,3,2,3,11,0,99,30,40,50", "3500,9,10,70,2,3,11,0,99,30,40,50");
 
     testIO("3,0,4,0,99", "1,0,4,0,99", "1");
+
+    testOpcode("1002", [2, 0, 1, 0])
+
+    const input = fs.readFileSync('day5_input.txt', 'utf8');
+    const [mem, out] = compileAndRun(input, 1);
+    console.log("out", out);;
+}
+
+function testOpcode(instruction, expected) {
+    console.log("testing opcode", instruction);
+    assert.deepStrictEqual(parseOpcode(instruction), expected);
 }
 
 function testIO(program, expectedMem, expectedOut) {
@@ -39,14 +50,22 @@ function compileAndRun(program, input) {
 
 // mem is an array. input is an int. 
 // this function is going to mutate the values of mem.
+// 
+// this is essentially a computer, so it dispenses with things like "good"
+// naming and DRY code in the interest of being as abstraction-free as possible
+// so a human can easily verify its correctness.
 function runProgram(mem, input) {
     const out = [];
     let ptr = 0;
     let stop = false;
-    let a, b, c;
+    // These are reusable registers that mean different things
+    // depending on the opcode, so don't waste any time naming them.
+    let a, b, c, d, e;
     while(!stop) {
-        const opcode = mem[ptr];
-        console.log("opcode", opcode);
+        const instruction = mem[ptr];
+        console.log("instruction", instruction);
+        const [opcode, ...modes] = parseOpcode(instruction);
+        console.log("opcode, modes", opcode, modes);
         switch(opcode) {
             case 99:
                 stop = true;
@@ -55,15 +74,18 @@ function runProgram(mem, input) {
                 a = mem[ptr+1];
                 b = mem[ptr+2];
                 c = mem[ptr+3];
-                console.log("adding", a, b, c);
-                mem[c] = mem[a] + mem[b];
+                d = resolve(a, mem, modes[0]);
+                e = resolve(b, mem, modes[1]);
+                mem[c] = d + e;
                 ptr += 4;
                 break;
             case 2:
                 a = mem[ptr+1];
                 b = mem[ptr+2];
                 c = mem[ptr+3];
-                mem[c] = mem[a] * mem[b];
+                d = resolve(a, mem, modes[0]);
+                e = resolve(b, mem, modes[1]);
+                mem[c] = d * e;
                 ptr += 4;
                 console.log("multiplying", a, b, c);
                 break;
@@ -75,7 +97,8 @@ function runProgram(mem, input) {
                 break;
             case 4: 
                 a = mem[ptr+1];
-                out.push(mem[a]);
+                b = resolve(a, mem, modes[0]);
+                out.push(b);
                 ptr += 2;
                 console.log("output", a)
                 break;
@@ -84,4 +107,33 @@ function runProgram(mem, input) {
         }
     }
     return out;
+}
+
+function resolve(param, mem, mode) {
+    if (mode === 0) {
+        return mem[param];
+    } else if (mode === 1) {
+        return param;
+    } else {
+        throw new Error("unexpected mode " + mode);
+    }
+}
+
+// Modify the value of `raw` as we go because I actually think it's less
+// confusing than making naming new variables. Also, don't bother with a loop
+// becuase we're only doing it 3 times so it's not worth the risk of messing
+// up the bounds. 
+function parseOpcode(raw) {
+    const opcode = raw % 100;
+    raw = Math.floor(raw / 100);
+    const modes = [];
+    modes.push(raw % 10);
+    raw = Math.floor(raw / 10);
+    modes.push(raw % 10);
+    raw = Math.floor(raw / 10);
+    modes.push(raw % 10);
+    raw = Math.floor(raw / 10);
+    // I'm using the spread operator here even though I immediately undo it
+    // because it makes the tests easier.
+    return [opcode, ...modes];
 }
